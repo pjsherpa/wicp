b864b750c3edd62f9bf546d1df25e1ce5bde0db4
Phinjock Sherpa <phinjock@gmail.com>
2022-12-14 14:19:15 -0800

    navbar created with burger features using material ui WIP home

---
+++ src/component/Burger.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Burger = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const PAGES = ["Home", "About us", "Travel Through", "Contact Us"];
+  return (
+    <React.Fragment>
+      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
+        <List>
+          {PAGES.map((page, index) => (
+            <ListItemButton key={index}>
+              <ListItemIcon>
+                <ListItemText>{page}</ListItemText>
+              </ListItemIcon>
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+      <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+        <MenuIcon />
+      </IconButton>
+    </React.Fragment>
+  );
+};
+
+export default Burger;
---
+++ src/component/Navbar.js
@@ -0,0 +1,72 @@
+import {
+  AppBar,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+  Button,
+} from "@mui/material";
+import React, { useState } from "react";
+import ConnectingAirportsIcon from "@mui/icons-material/ConnectingAirports";
+import Burger from "./Burger";
+
+const Navbar = () => {
+  const [value, setValue] = useState();
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  console.log(isMatch);
+  const PAGES = ["Home", "About us", "Travel Through", "Contact Us"];
+
+  return (
+    <React.Fragment>
+      <AppBar sx={{ background: "#AF601A" }}>
+        <Toolbar>
+          <ConnectingAirportsIcon />
+          {isMatch ? (
+            <>
+              <Typography>WICP</Typography>
+              <Burger />
+            </>
+          ) : (
+            <>
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, value) => setValue(value)}
+                indicatorColor="primary"
+              >
+                {PAGES.map((page, index) => (
+                  <Tab key={index} label={page} />
+                ))}
+              </Tabs>
+              <Button
+                sx={{
+                  marginLeft: "auto",
+                  background: "#F7F9F9",
+                  color: "black",
+                }}
+                variant="contained"
+              >
+                Login
+              </Button>
+              <Button
+                sx={{
+                  marginLeft: "10px",
+                  background: "#85929E",
+                  color: "black",
+                }}
+                variant="contained"
+              >
+                Signup
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
